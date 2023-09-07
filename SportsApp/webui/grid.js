85bcb37c87ade9c0324702917ec48dc217e1e9a9
Rahul Kumar <rahulk@progress.com>
2023-09-07 22:29:06 +0530

    fix sports app url

--- SportsApp/webui/grid.js
+++ SportsApp/webui/grid.js
@@ -4,7 +4,7 @@
 
 $(function () {
     'use strict';
-    var serviceURI = "https://localhost:8811/Sports";
+    var serviceURI = "https://ec2-54-161-12-110.compute-1.amazonaws.com:8811/Sports";
     var catalogURI = serviceURI + "/static/SportsService.json";
 
     function createGrid() {
