60aaddf827e34050392db1fb1a250791b6e04b9f
Rahul Kumar <rahulk@progress.com>
2023-09-07 21:24:24 +0530

    fix sports app url

--- SportsApp/webui/grid.js
+++ SportsApp/webui/grid.js
@@ -4,7 +4,7 @@
 
 $(function () {
     'use strict';
-    var serviceURI = "https://<Place_IP_Address_of_Docker_Host>:8811/Sports";
+    var serviceURI = "https://localhost:8811/Sports";
     var catalogURI = serviceURI + "/static/SportsService.json";
 
     function createGrid() {
