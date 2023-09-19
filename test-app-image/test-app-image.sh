1c95eabe2439c0413bfa55009beca967d388ce15
Rahul Kumar <rahulk@progress.com>
2023-09-20 00:35:17 +0530

    Use correct node

--- test-app-image/test-app-image.sh
+++ test-app-image/test-app-image.sh
@@ -2,6 +2,7 @@ echo "##teamcity[testStarted name='Test_MasterSample_DataObject']"
 
 # Run Test using NodeJS
 # export PATH=$PATH:/tools/linuxx86_64/nodejs/node-v6.9.1/bin
+nvm use 6.9.1
 npm install
 
 node --version
