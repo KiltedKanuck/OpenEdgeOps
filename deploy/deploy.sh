0fe55e071efbf363685eb283769d653eb71c4dc4
Rahul Kumar <rahulk@progress.com>
2023-09-20 21:25:43 +0530

    debug deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,22 +2,15 @@
 
 
 # env vars
-# export DOCKER_REPO_URL=${DOCKER_REPO_URL}
-# export OE_VERSION=${OE_VERSION}
-# export APP_NAME=${APP_NAME}
-# export APP_VERSION=${APP_VERSION}
-# export APP_GROUP=${APP_GROUP}
-
-# export APP_GROUP=progress/rahulk
-# export APP_NAME=sports
-# export APP_VERSION=latest
-# export DOCKER_REPO_URL=ec2-54-80-142-101.compute-1.amazonaws.com:9443
-# export OE_VERSION=12.8.0
+export DOCKER_REPO_URL=${DOCKER_REPO_URL}
+export OE_VERSION=${OE_VERSION}
+export APP_NAME=${APP_NAME}
+export APP_VERSION=${APP_VERSION}
+export APP_GROUP=${APP_GROUP}
 
 # create the app docker image
 # docker build --no-cache -t sports:latest .
 
 # deploy
 PAS_INSTANCE_NAME=oepas1
-docker-compose -p ${PAS_INSTANCE_NAME}  up -d
-echo "PASOE instance named '${PAS_INSTANCE_NAME}_dc' will be available at 'https://localhost:8811'"
+docker-compose -p ${PAS_INSTANCE_NAME} up -d --verbose
--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # env vars
-# export DOCKER_REPO_URL=${DOCKER_REPO_URL}
-# export OE_VERSION=${OE_VERSION}
-# export APP_NAME=${APP_NAME}
-# export APP_VERSION=${APP_VERSION}
-# export APP_GROUP=${APP_GROUP}
+export DOCKER_REPO_URL=${DOCKER_REPO_URL}
+export OE_VERSION=${OE_VERSION}
+export APP_NAME=${APP_NAME}
+export APP_VERSION=${APP_VERSION}
+export APP_GROUP=${APP_GROUP}
 
 # undeploy
 PAS_INSTANCE_NAME=oepas1
-docker-compose -p ${PAS_INSTANCE_NAME} down -v
-echo "Removed '${PAS_INSTANCE_NAME}_dc'"
+docker-compose -p ${PAS_INSTANCE_NAME} down -v --verbose
