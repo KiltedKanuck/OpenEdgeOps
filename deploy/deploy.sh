3b4e649d609612b7a82e87620f4b28fd37ef30e8
Rahul Kumar <rahulk@progress.com>
2023-09-20 21:37:37 +0530

    debug deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,6 +8,12 @@ export APP_NAME=${APP_NAME}
 export APP_VERSION=${APP_VERSION}
 export APP_GROUP=${APP_GROUP}
 
+echo "DOCKER_REPO_URL=${DOCKER_REPO_URL}"
+echo "OE_VERSION=${OE_VERSION}"
+echo "APP_NAME=${APP_NAME}"
+echo "APP_VERSION=${APP_VERSION}"
+echo "APP_GROUP=${APP_GROUP}"
+
 # create the app docker image
 # docker build --no-cache -t sports:latest .
 
