6f141482f5402d730b55e544c0e71065e2c99df7
Rahul Kumar <rahulk@progress.com>
2023-09-20 20:46:37 +0530

    use env vars for deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# env vars
+export DOCKER_REPO_URL=${DOCKER_REPO_URL}
+export OE_VERSION=${OE_VERSION}
+export APP_NAME=${APP_NAME}
+export APP_VERSION=${APP_VERSION}
+export APP_GROUP=${APP_GROUP}
+
 # create the app docker image
 # docker build --no-cache -t sports:latest .
 
